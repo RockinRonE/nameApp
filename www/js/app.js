4c0efae8e39db048c75a87a94b90b91e70b4dfb2
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-03-29 17:50:36 -0500

    fished add task feature

--- www/js/app.js
+++ www/js/app.js
@@ -22,3 +22,37 @@ angular.module('todo', ['ionic'])
     }
   });
 })
+
+.controller('TodoCtrl', function($scope, $ionicModal) {
+  // no need for testing data anymore
+  $scope.tasks = [];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal){
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide(); 
+    task.title = "";
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show(); 
+  };
+
+  // Close out new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide(); 
+  };
+
+
+});
